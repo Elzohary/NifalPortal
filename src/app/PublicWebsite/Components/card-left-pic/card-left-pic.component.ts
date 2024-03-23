ee152731095094355e551d8ca0a72fd8495e533c
Mohamed Elzohary <96190296+Elzohary@users.noreply.github.com>
2024-03-23 03:50:59 +0300

    stablePublic

--- src/app/PublicWebsite/Components/card-left-pic/card-left-pic.component.ts
+++ src/app/PublicWebsite/Components/card-left-pic/card-left-pic.component.ts
@@ -1,12 +1,11 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { FocusStickyCardsService } from '../../Services/focus-sticky-cards.service';
 
 @Component({
   selector: 'app-card-left-pic',
   standalone: true,
   imports: [],
   templateUrl: './card-left-pic.component.html',
-  styleUrl: './card-left-pic.component.scss'
+  styleUrls: ['./card-left-pic.component.scss']
 })
 export class CardLeftPicComponent {
   
@@ -15,18 +14,6 @@ export class CardLeftPicComponent {
   @ViewChild('cardLeftSectoin', { static: false })
   cardLeftSectoin!: ElementRef;
 
-  constructor(private isInViewService : FocusStickyCardsService) {}
+  constructor() {}
 
-  ngAfterViewInit(): void {
-    this.checkIfSectionInView(); // Call the check method after view initialization
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  checkIfSectionInView(event?: Event): void {
-    if (this.cardLeftSectoin) {
-      const sectionRect = this.cardLeftSectoin.nativeElement.getBoundingClientRect();
-      this.isInViewService.isSectionInView = sectionRect.top <= window.innerHeight && sectionRect.bottom >= 0;
-      this.isInViewService.sharedData = sectionRect.top <= window.innerHeight && sectionRect.bottom >= 0; // obs
-    }
-  }
 }
